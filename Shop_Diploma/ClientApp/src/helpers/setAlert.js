a1b6627a698d626f2f3e35e8e72fa9053630920d
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-11-27 22:29:28 +0200

    update alert css, change alert added

--- Shop_Diploma/ClientApp/src/helpers/setAlert.js
+++ Shop_Diploma/ClientApp/src/helpers/setAlert.js
@@ -1,20 +1,15 @@
-let counter = 0;
-
 export const setAlert = (props) => {
 
-    counter++;
     const alert = document.getElementById('alert');
 
     let element = document.createElement('div');
     element.className = `alert alert-${props.type} alert-fade`;
-    element.style.top = (75 * counter) + 'px';
     element.innerHTML = `${props.type === 'danger' ? '<i class="fa fa-exclamation-circle"></i>' :
         '<i class="fa fa-check-circle"></i>'}${props.message}`;
 
-    alert.append(element);
+    alert.prepend(element);
 
-    const alertFadeLength = document.getElementsByClassName('alert-fade').length - 1;
-    const alertFade = document.getElementsByClassName('alert-fade')[alertFadeLength];
+    const alertFade = document.getElementsByClassName('alert-fade')[0];
 
     const timeout_id = setTimeout(() => {
         alertFade.classList.add('remove');
